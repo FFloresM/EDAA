f4da4ca3cc7e9e90bd3990f8a0dd9f88d93faeb4
Francisco <victoriafrancis2009@gmail.com>
2019-08-20 18:29:44 -0400

    lab 1

---
+++ lab_1/busqueda.py
@@ -0,0 +1,15 @@
+
+def secuencial(vector, elem):
+	for i in vector:
+		if i == elem:
+			return True
+
+def binaria(vector, elem):
+	"""vector must be sorted"""
+	i = 0
+	d = len(vector)
+	n = (d-i)/2
+	
+
+	
+	
