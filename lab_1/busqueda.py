701fe328e4a90f2cdb5d0c12e07131f955630c61
Francisco Flores <ffloresm@inf.udec.cl>
2019-08-23 14:20:15 -0400

    BB done

--- lab_1/busqueda.py
+++ lab_1/busqueda.py
@@ -3,18 +3,30 @@ def secuencial(vector, elem):
 	for i in vector:
 		if i == elem:
 			return True
+	return False
 
 def binaria(vector, elem):
 	"""vector must be sorted"""
 	i = 0
-	d = len(vector)
-	while 1:
-		n = (d-i)/2
+	d = len(vector)-1
+	while i<=d:
+		n = int((d+i)/2)
 		if vector[n] == elem:
 			return True
-		elif n > elem:
+		elif vector[n] > elem:
+			d = n+1
+		elif vector[n] < elem:
+			i = n+1
+	return False
 			
 
+v = []
+for i in range(1,101):
+	v.append(i)
+
+T = 102
+print(secuencial(v,T))
+print(binaria(v,T))
 
 	
 	
