41353129fa9ba80f26973e2bb3d93ef3bc26e68f
Francisco Flores <victoriafrancis2009@gmail.com>
2019-09-05 15:04:46 -0400

    splay insert

--- lab_2/Splay.java
+++ lab_2/Splay.java
@@ -2,8 +2,15 @@ import BST
 
 public class SplayTree extends BST{
 
+	Nodo root;
 	
 	SplayTree(){
+
+	}
+
+	void insert(int k){
+		root = super.insert(int k);
+		//agregar splays al insertar
 
 	}
 }
