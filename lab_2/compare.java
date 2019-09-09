ca8302969d2dc1a6e35577515b08c2dc65ba630d
Francisco Flores <victoriafrancis2009@gmail.com>
2019-09-09 16:18:28 -0300

    compare random

--- lab_2/compare.java
+++ lab_2/compare.java
@@ -1,6 +1,9 @@
 import java.util.TreeMap;
 import java.util.List;
 import java.util.ArrayList;
+//numeros aleatorios
+import java.util.concurrent.ThreadLocalRandom;
+
 
 class bst_and_splay_compare{
 
@@ -8,6 +11,9 @@ class bst_and_splay_compare{
 
 		Bst bst = new Bst();
 		SplayTree spt = new SplayTree();
+
+		int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+		
 
 		//crear list con valores aleatorios.
 		//insertar valores y buscar. comparar
